558156b5a2a83ac56bbc046319f26eda7f865d1c
Minyoung Shin <mins6649@gmail.com>
2021-08-03 14:33:40 -0400

    creating an object

---
+++ src/00-objectball.js
@@ -0,0 +1,59 @@
+const gameObject = {
+    home = {
+        teamName = "Brooklyn Nets",
+        teamColor = "Black, White",
+        players = {
+            Alan_Anderson = {
+                number = 0,
+                shoe = 16,
+                points = 22,
+                rebounds = 12,
+                assists = 12,
+                steals = 3,
+                blocks = 1,
+                slamDunks = 1,
+             },
+             Reggie_Evans = {
+                number = 30,
+                shoe = 14,
+                points = 12,
+                rebounds = 12,
+                assists = 12,
+                steals = 12,
+                blocks = 12,
+                slamDunks = 7,
+             }
+            
+        }
+
+    },
+
+    away = {
+        teamName = "Charlotte Hornets",
+        teamColor = "Turquoise, Purple",
+        players = {
+            Jeff_Adrien = {
+                number = 4,
+                shoe = 18,
+                points = 10,
+                rebounds = 1,
+                assists = 1,
+                steals = 2,
+                blocks = 7,
+                slamDunks = 2,
+             },
+             Bismak_Biyombo = {
+                number = 4,
+                shoe = 18,
+                points = 10,
+                rebounds = 1,
+                assists = 1,
+                steals = 2,
+                blocks = 7,
+                slamDunks = 2,
+
+        },
+
+    },
+}
+}
